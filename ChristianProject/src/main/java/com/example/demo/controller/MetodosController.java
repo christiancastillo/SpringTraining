46b7d5d7d71438c2178b91efdc47bdc2f5e7d248
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2022-05-31 16:12:29 -0500

    .

--- ChristianProject/src/main/java/com/example/demo/controller/MetodosController.java
+++ ChristianProject/src/main/java/com/example/demo/controller/MetodosController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,8 +27,26 @@ public class MetodosController {
 	//Metodo dos
 	@GetMapping("/metodoDos")
 	public ModelAndView MetodoDos() {
-		ModelAndView datos = new ModelAndView("vista_dos"); //se define la vista desde donde se hara binding
-		datos.addObject("persona", new Persona("Aleida", 55112233));
-		return new ModelAndView("vista_dos"); //retorna ModelAndView vista_dos.html
+		ModelAndView mav = new ModelAndView("vista_dos"); //se define la vista desde donde se hara binding
+		mav.addObject("persona", new Persona("Aleida Torres", 55112233)); //se agrega la llave y la informacion que contiene dicha llave
+		return mav; //retorna ModelAndView vista_dos.html
+	}
+	
+	@GetMapping("/listaPersonas")
+	public String MetodoTres(Model modelo) {
+		modelo.addAttribute("p",getPersonas());
+		return "lista_personas";
+	}
+	
+	
+	//Obtiene un listado de personas
+	private List<Persona> getPersonas(){
+		List<Persona> personaList = new ArrayList<>();
+		personaList.add(new Persona("Christian Yamil Castillo C", 771214));
+		personaList.add(new Persona("Aleida Torres Sanchez", 772736));
+		personaList.add(new Persona("Dafne Castillo Torres",112233));
+		personaList.add(new Persona("Lety Covarrubias Lopez", 7766222));
+		personaList.add(new Persona("Fermin Castillo Gonzalez", 77112233));
+		return personaList;				
 	}
 }
---
+++ ChristianProject/src/main/java/com/example/demo/controller/PeticionesController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class PeticionesController {
+
+	//URL: https://www.youtube.com/watch?v=DBoqqWHmKKk&list=PLz8JVd0sHx7p7bqv9kGANmpMaSw9tYoiv&index=6
+	public ModelAndView peticiones() {
+		ModelAndView mav = new ModelAndView();
+		
+	}
+}
