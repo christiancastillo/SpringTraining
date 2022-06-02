0f1bd07a90de1c4a6909676ca01afe742baf53b5
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2022-06-01 20:35:11 -0500

    .

--- ChristianProject/src/main/java/com/example/demo/controller/PeticionesController.java
+++ ChristianProject/src/main/java/com/example/demo/controller/PeticionesController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,8 +12,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class PeticionesController {
 
 	//URL: https://www.youtube.com/watch?v=DBoqqWHmKKk&list=PLz8JVd0sHx7p7bqv9kGANmpMaSw9tYoiv&index=6
-	public ModelAndView peticiones() {
-		ModelAndView mav = new ModelAndView();
-		
+	//localhost:8080/peticiones1?nombre=María
+	@GetMapping("/peticiones1")
+	public ModelAndView peticiones(@RequestParam(name="nombre") String _nombre) {
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("nombre",_nombre);
+		return mav;				
+	}
+	
+	//localhost:8080/peticiones1/María
+	
+	@GetMapping("/peticiones2/{nombre}")
+	public ModelAndView peticionesDos(@PathVariable(name="nombre") String _nombre) {
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("nombre", _nombre);
+		return mav;
 	}
 }
