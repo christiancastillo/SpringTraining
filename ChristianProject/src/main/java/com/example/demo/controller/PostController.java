74d603c8d9780c4a1ae48248fca3e4bc5a5cb9b9
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2022-06-04 18:54:31 -0500

    .

--- ChristianProject/src/main/java/com/example/demo/controller/PostController.java
+++ ChristianProject/src/main/java/com/example/demo/controller/PostController.java
@@ -3,7 +3,10 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.model.Persona;
 
@@ -15,5 +18,15 @@ public class PostController {
 	public String form(Model model) {
 		model.addAttribute("persona", new Persona());
 		return "formulario";
+	}
+	
+	@PostMapping("/addPersona") //se pone el método de th:action del formulario 
+	public ModelAndView resultados(@ModelAttribute("persona") Persona persona) {
+		//return new ModelAndView("resultados").addObject("persona", persona);
+		ModelAndView mav = new ModelAndView("resultados");
+		mav.addObject("persona", persona);
+		return mav;
+		
+		
 	}
 }
