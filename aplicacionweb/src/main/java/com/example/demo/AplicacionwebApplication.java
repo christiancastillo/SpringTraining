f3464bee828d4e36ffcf27102059cb94207946e7
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2022-06-12 00:30:43 -0500

    tutorial aplicacion web de youtube

---
+++ aplicacionweb/src/main/java/com/example/demo/AplicacionwebApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionwebApplication {
+
+	//aplicacion web con spring: https://www.youtube.com/watch?v=15sit3XQNbY&list=PLz8JVd0sHx7p7bqv9kGANmpMaSw9tYoiv&index=13
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionwebApplication.class, args);
+	}
+
+}
